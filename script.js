f3acb8348520b2169a9e78d4b9b2e6fac7e5f75b
juncards <38598318+juncards@users.noreply.github.com>
2024-12-16 19:18:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form submission event handler
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+});
